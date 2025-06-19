30706ef9074dce0c53b5d295f9035bd29d4abf4f
Bruno Unna <bruno.unna@gmail.com>
2025-06-19 19:44:00 +0100

    Implement the `distance` function

--- src/historian_hysteria.py
+++ src/historian_hysteria.py
@@ -1,2 +1,9 @@
-def distance(left: list[int], right: list[int])  -> int:
-    return 0
+def distance(left: list[int], right: list[int]) -> int | None:
+    def subtract_tuple_elements(tup) -> int:
+        return abs(tup[0] - tup[1])
+
+    if len(left) != len(right):
+        return None
+    tuples = zip(sorted(left), sorted(right))
+    diffs = map(subtract_tuple_elements, tuples)
+    return sum(diffs)
