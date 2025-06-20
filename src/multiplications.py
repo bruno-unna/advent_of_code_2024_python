a9de13e8b21379b3d7080a523a613564a500470f
Bruno Unna <bruno.unna@gmail.com>
2025-06-20 20:59:56 +0100

    Implement memory_multiply

--- src/multiplications.py
+++ src/multiplications.py
@@ -1,3 +1,12 @@
-def memory_multiply(memory: str) -> int:
+import re
 
-    return 0
+
+def memory_multiply(memory: str) -> int:
+    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+    pairs = re.findall(pattern, memory)
+    acc = 0
+    pair: tuple[int, int]
+    for pair in pairs:
+        factor1, factor2 = pair
+        acc += int(factor1) * int(factor2)
+    return acc
