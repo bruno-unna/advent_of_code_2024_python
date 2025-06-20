cfb04e91c6d93e8327f9204e65fa18acb63bb9f7
Bruno Unna <bruno.unna@gmail.com>
2025-06-20 13:44:06 +0100

    Implement count_safe_reports

--- src/red_nosed_reports.py
+++ src/red_nosed_reports.py
@@ -1,2 +1,14 @@
+from itertools import pairwise
+
+
 def count_safe_reports(reports: list[list[int]]) -> int:
-    return 0
+    def is_safe(record: list[int]) -> bool:
+        diffs = [b - a for a, b in pairwise(record)]
+        result = True
+        if any(x == 0 or x < -3 or x > 3 for x in diffs):
+            result = False
+        if any(x < 0 for x in diffs) and any(x > 0 for x in diffs):
+            result = False
+        return result
+
+    return sum(1 for record in reports if is_safe(record))
