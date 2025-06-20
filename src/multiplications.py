ccf675f3e360e40a1f86e0d6564d2a463e2940bc
Bruno Unna <bruno.unna@gmail.com>
2025-06-20 22:49:26 +0100

    Simplify the implementation of selective multiplication

--- src/multiplications.py
+++ src/multiplications.py
@@ -16,12 +16,4 @@ def memory_multiply(memory: str) -> int:
 def selective_memory_multiply(memory: str) -> int:
     chunks = memory.split('do()')
     reduced_memory = reduce(lambda a, b: a + b, map(lambda x: x.split("don't()")[0], chunks))
-
-    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
-    pairs = re.findall(pattern, reduced_memory)
-    acc = 0
-    pair: tuple[int, int]
-    for pair in pairs:
-        factor1, factor2 = pair
-        acc += int(factor1) * int(factor2)
-    return acc
+    return memory_multiply(reduced_memory)
