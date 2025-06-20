67e001ab62beba59d3e4f3a6920d0465f6d50507
Bruno Unna <bruno.unna@gmail.com>
2025-06-20 18:39:54 +0100

    Fix the is_safe_dampened method

--- src/red_nosed_reports.py
+++ src/red_nosed_reports.py
@@ -2,8 +2,8 @@ from itertools import pairwise
 
 
 def count_safe_reports(reports: list[list[int]]) -> dict[str, int]:
-    def is_safe(record: list[int]) -> bool:
-        diffs = [b - a for a, b in pairwise(record)]
+    def is_safe(report: list[int]) -> bool:
+        diffs = [b - a for a, b in pairwise(report)]
         result = True
         if any(x == 0 or x < -3 or x > 3 for x in diffs):
             result = False
@@ -11,8 +11,16 @@ def count_safe_reports(reports: list[list[int]]) -> dict[str, int]:
             result = False
         return result
 
-    def is_safe_dampened(record: list[int]) -> bool:
-        return True
+    def is_safe_dampened(report: list[int]) -> bool:
+        if is_safe(report):
+            return True
+        fixable = False
+        for index in range(0, len(report)):
+            reduced_report = report[:index] + report[index + 1:]
+            if is_safe(reduced_report):
+                fixable = True
+                break
+        return fixable
 
     return {"undampened": sum(1 for record in reports if is_safe(record)),
             "dampened": sum(1 for record in reports if is_safe_dampened(record))}
